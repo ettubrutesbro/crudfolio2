3e04ab98c6e772b7c745ca36145732055056a212
jack L <Jack.Leng@gmail.com>
2026-01-26 04:04:59 -0800

    cleanup, test hover styles

--- src/components/Floater.jsx
+++ src/components/Floater.jsx
@@ -1,36 +1,40 @@
 import { useState, useRef, useEffect, useMemo, useLayoutEffect } from 'react'
 import { motion, useMotionValue, useTransform, animate } from 'motion/react'
 import clsx from 'clsx'
+
+import { useDuck } from '../App'
 import styles from './Floater.module.css'
 
-const Floater = ({ isOpen, onClose, name, blurb, isTall, img, rowRect, i, xOrigin, ...props }) => { //clean up later
+const Floater = ({ isOpen, onClose, name, blurb, img, rowRect, i, xOrigin, ...props }) => { //clean up later
 
   const [maskOn, setMask] = useState(true)
   const [floaterHeight, setFloaterHeight] = useState(0)
 
   const containerRef = useRef()
   const floaterRef = useRef()
-
+  const isTall = img?.height > img?.width
   const floaterWidth = isTall? 200 : 300
+  const setFloaters = useDuck((state) => state.setFloaters)
+  const topThird = rowRect.top <= window.innerHeight/3
+
+  const imageSrc = img?.name ? `assets/thumbnails/${img.name}.png` : null
+
+
   useLayoutEffect(() =>  {
     const rect = floaterRef.current.getBoundingClientRect()
     console.log(`layout effect: floater height is ${rect.height}`)
     setFloaterHeight(rect.height)
   }, [])
 
-  const imageSrc = img?.name ? `assets/thumbnails/${img.name}.png` : null
 
-  const topThird = rowRect.top <= window.innerHeight/3
 
   const floaterStart = useMemo(() => {
     console.log('float recalc')
     if (!rowRect) return { x: '', y: '' }
-    
     const clampedX = Math.max(
       floaterWidth / 2, 
       Math.min(xOrigin, rowRect.width - floaterWidth / 2)
     )
-    console.log(-floaterHeight)
     return {
       x: rowRect.left + clampedX - floaterWidth / 2,
       y: !topThird? rowRect.top + window.scrollY + rowRect.height : -floaterHeight
@@ -47,10 +51,16 @@ const Floater = ({ isOpen, onClose, name, blurb, isTall, img, rowRect, i, xOrigi
     }
   }, [])
 
+  const handleClose = () => {
+    setFloaters({id: props.id})
+  }
+
   //calculate offsets (position after animation) based on scrollposition vs. row position
   //and on dimension of floater + a randomized offset which determines direction and distance 
   const xOffset = 100
   const yOffset = !topThird? floaterHeight + 25 : -floaterHeight - 25
+  const rotation = !topThird && isTall? -90 : !topThird? -25 : isTall? 90 : 45
+
 
   return (
     <div
@@ -61,8 +71,8 @@ const Floater = ({ isOpen, onClose, name, blurb, isTall, img, rowRect, i, xOrigi
         left:  maskData.x,
         top: maskData.y,
         height: maskData.height,
-        // border: maskOn? '1px solid red' : 'none',
         clipPath: maskOn? 'inset(0 0 0 0)' : 'none',
+        // border: maskOn? '1px solid red' : 'none',
         // background: maskOn? 'rgba(255,0,0,0.1)' : ''
       }}
     >
@@ -71,39 +81,33 @@ const Floater = ({ isOpen, onClose, name, blurb, isTall, img, rowRect, i, xOrigi
         ref={floaterRef}
         className={clsx(styles.floater, isTall && styles.tall)}
         style = {{
-          //these values are where it should start, so begin the
-          //TODO: subtract half of floater width and constrain X
-          // left: rowRect? rowRect.left + (Math.max(floaterWidth/2, Math.min(xOrigin,rowRect.width-floaterWidth/2)) - floaterWidth/2) : '',
-          // top: rowRect? rowRect.top + window.scrollY + rowRect.height - 100 : '',
           background: props.colors.bg || '',
           color: props.colors.text || '',
           left: floaterStart.x,
           top: floaterStart.y
         }}
-        initial = {{x: 0, y: 0}}
-        // animate = {{y: floaterStart.x + xOffset, y: floaterStart.y + yOffset}}
+        initial = {{x: 0, y: 0, rotate: !topThird? -10 : 45}}
         animate = {{
           x: xOffset,
-          y: -yOffset
+          y: -yOffset,
+          rotate: 0
         }}
-          transition={{
-  x: {
-    duration: 0.6,
-    ease: [0.43, 0.13, 0.23, 0.96]  // Custom bezier
-  },
-  y: {
-    duration: 0.4,
-    ease: [0.22, 0.61, 0.36, 1]
-  }
-}}
+        transition={{
+          x: {
+            duration: 0.6,
+            ease: [0.43, 0.13, 0.23, 0.96]  // Custom bezier
+          },
+          y: {
+            duration: 0.4,
+            ease: [0.22, 0.61, 0.36, 1]
+          }
+        }}
         onAnimationComplete = {()=>{setMask(false)}}
-        // TODO: callback when animation completes..? prev. done with state and animate method
-        // transition
       >
         <header>
           <button
             className={styles.closeButton}
-            // onClick={handleClose}
+            onClick={handleClose}
           >
             ×
           </button>
